7edcd6bb97981632c1c89e339f2c2efb14a1f3bf
today0309 <605816863@qq.com>
2016-06-22 16:15:11 +0800

    test

---
+++ protected/controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TestController
+ *
+ * @author Administrator
+ */
+class TestController extends Controller {
+    
+    public function actionTest() {
+        echo 3333;
+        exit;
+    }
+}
