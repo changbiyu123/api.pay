e708f5c54b293137dff3910c6afde2a92191aa40
today0309 <605816863@qq.com>
2016-06-24 16:12:41 +0800

    test

--- protected/controllers/WechatpayController.php
+++ protected/controllers/WechatpayController.php
@@ -14,54 +14,60 @@ class WechatpayController extends Controller {
     
     public function actionTest(){
         echo 12345;
-        exit;
-        //Yii::app()->exit();
+        Yii::app()->end();
     }
     
     /**
      * 微信公众号支付
      */
     public function actionJsapipay(){
-        $reqStr = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : file_get_contents("php://input");
-        $reqJson = json_decode($reqStr, true);
+        $output = new stdClass();
         
+        $reqStr = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : file_get_contents("php://input");
+        $reqJson = json_decode($reqStr, true);       
+        if(!isset($reqJson['out_trade_no']) || !isset($reqJson['total_fee']) || !isset($reqJson['openid']) || !isset($reqJson['body'])){
+            $output->flag = 1;
+            $output->info = 'missing payment parameters';
+            return $this->renderJsonOutput($output);
+        }
+
         $out_trade_no = $reqJson['out_trade_no'];//商户系统内部的订单号,32个字符内、可包含字母
         $total_fee = $reqJson['total_fee'];//订单总金额，单位为分
         $openid = $reqJson['openid'];//trade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识
         $body = $reqJson['body'];//商品或支付单简要描述
-        
+
         $input = new WxPayUnifiedOrder();
         $input->SetBody($body);//商品或支付单简要描述
         $input->SetOut_trade_no($out_trade_no);//商户系统内部的订单号,32个字符内、可包含字母
         $input->SetTotal_fee($total_fee);//订单总金额，单位为分
-        $input->SetOpenid($openid);//trade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识
-        
+        $input->SetOpenid($openid);//trade_type=JSAPI，此参数必传，用户在商户appid下的唯一标识        
         $input->SetNotify_url("http://".$_SERVER['HTTP_HOST']."/weixinpub/wechatpay/callback");//接收微信支付异步通知回调地址，通知url必须为直接可访问的url，不能携带参数。
         $input->SetTrade_type("JSAPI");//交易类型
         $input->SetAppid(APPID);//公众账号ID
         $input->SetMch_id(MCHID);//商户号
-               
-        $order = WxPayApi::unifiedOrder($input);
-        $jsApiParameters = $this->GetJsApiParameters($order);
         
-        $output = new stdClass();
-        $output->rspStr = $jsApiParameters;        
+        try {
+            $order = WxPayApi::unifiedOrder($input);
+            $jsApiParameters = $this->GetJsApiParameters($order);
+            $output->flag = 0;
+            $output->info = $jsApiParameters; 
+        } catch (Exception $exc) {
+            $output->flag = 1;
+            $output->info = 'system exceptions';
+        }
+        
         return $this->renderJsonOutput($output);
     }
     
     
    /**
-    * 
     * 获取jsapi支付的参数
     * @param array $UnifiedOrderResult 统一支付接口返回的数据
-    * @throws WxPayException
-    * 
     * @return json数据，可直接填入js函数作为参数
     */
     public function GetJsApiParameters($UnifiedOrderResult){
         if(!array_key_exists("appid", $UnifiedOrderResult) || !array_key_exists("prepay_id", $UnifiedOrderResult) || $UnifiedOrderResult['prepay_id'] == ""){
-            echo var_export($UnifiedOrderResult);
-            Yii::app()->end();
+            return var_export($UnifiedOrderResult);
         }
         $jsapi = new WxPayJsApiPay();
         $jsapi->SetAppid($UnifiedOrderResult["appid"]);
