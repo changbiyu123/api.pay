0bda3956d995168521260827eefeefa7c2a1961f
today0309 <605816863@qq.com>
2016-06-29 14:42:41 +0800

    123

--- protected/controllers/WechatpayController.php
+++ protected/controllers/WechatpayController.php
@@ -123,8 +123,7 @@ class WechatpayController extends Controller {
     */
     public function GetJsApiParameters($UnifiedOrderResult){
         if(!array_key_exists("appid", $UnifiedOrderResult) || !array_key_exists("prepay_id", $UnifiedOrderResult) || $UnifiedOrderResult['prepay_id'] == ""){
-            //return var_export($UnifiedOrderResult);
-            return '返回异常';
+            return json_encode($UnifiedOrderResult);
         }
         $jsapi = new WxPayJsApiPay();
         $jsapi->SetAppid($UnifiedOrderResult["appid"]);
