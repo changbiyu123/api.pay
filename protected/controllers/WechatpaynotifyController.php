84f6c72ffda904db1a21e12bcd9a91ab02fd895f
today0309 <605816863@qq.com>
2016-06-30 10:27:08 +0800

    test

--- protected/controllers/WechatpaynotifyController.php
+++ protected/controllers/WechatpaynotifyController.php
@@ -16,7 +16,7 @@ class WechatpaynotifyController extends Controller {
         $xml = $GLOBALS['HTTP_RAW_POST_DATA'];
         Yii::log($xml);
         //将xml格式的数据转为数组
-        $arr = CommonConfig::FromXml($xml);
+        $arr = $this->FromXml($xml);
         //如果返回的状态码是SUCCESS，并且签名验证通过
         if($arr['return_code'] == 'SUCCESS' && $arr['sign'] == $this->MakeSign($arr)){
             //在这里进行数据页面处理，然后再返回数据
@@ -36,9 +36,11 @@ class WechatpaynotifyController extends Controller {
      * @param type $arr
      */
     public function MakeSign($arr){    
+        $wechatAccount = new WechatAccount();
+        $api_key = $wechatAccount->getByAppId($arr['appid'])->getApiKey();
         ksort($arr);//签名步骤一：按字典序排序参数        
         $string = CommonConfig::ToUrlParams($arr);//格式化参数       
-        $string = $string . "&key=".WxPayConfig::KEY;//签名步骤二：在string后加入KEY        
+        $string = $string . "&key=".$api_key;//签名步骤二：在string后加入KEY        
         $string = md5($string);//签名步骤三：MD5加密        
         $result = strtoupper($string);//签名步骤四：所有字符转为大写
         return $result;
@@ -50,7 +52,7 @@ class WechatpaynotifyController extends Controller {
      */
     public static function ToXml($arr){
         if(!is_array($arr) || count($arr) <= 0){
-            Yii::log("数组数据异常");
+            return null;
     	}    	
     	$xml = "<xml>";
     	foreach ($arr as $key=>$val){
@@ -63,6 +65,19 @@ class WechatpaynotifyController extends Controller {
         $xml.="</xml>";
         return $xml; 
     }
+    
+    /**
+     * 将xml转为array
+     * @param string $xml
+     */
+    public static function FromXml($xml){	
+        if(!$xml){
+            return null;
+        }       
+        libxml_disable_entity_loader(true);//禁止引用外部xml实体
+        $arr = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);		
+        return $arr;
+    } 
     
     
 }
